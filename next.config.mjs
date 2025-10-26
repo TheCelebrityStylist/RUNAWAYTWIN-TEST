88a9da3358f22cb0a78663cd0dbae6c8428b38a3
TheCelebrityStylist <elkejaspers.eac@gmail.com>
2025-10-26 22:54:48 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: { serverActions: { bodySizeLimit: '2mb' } },
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] }
 };
 
 export default nextConfig;
